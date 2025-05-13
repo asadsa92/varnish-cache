bf35ca857b461550bd425f033b16c6279cd73200
Asad Sajjad Ahmed <asadsa@varnish-software.com>
2025-05-13 14:08:29 +0200

    vrt: bump version to 21.1
    The vmodtool.py syntax version was accidentally bumped to 2.0.
    To make this change explicit, bump the VRT minor version.
    
    Signed-off-by: Asad Sajjad Ahmed <asadsa@varnish-software.com>

--- include/vrt.h
+++ include/vrt.h
@@ -48,7 +48,7 @@
 
 #define VRT_MAJOR_VERSION	21U
 
-#define VRT_MINOR_VERSION	0U
+#define VRT_MINOR_VERSION	1U
 
 /***********************************************************************
  * Major and minor VRT API versions.
@@ -57,6 +57,8 @@
  * Whenever something is deleted or changed in a way which is not
  * binary/load-time compatible, increment MAJOR version
  *
+ * 21.1 (2025-05-13)
+ *	vmodtool.py syntax version upgraded to 2.0
  * 21.0 (2025-03-17)
  *	VRT_u_req_grace() added
  *	VRT_u_req_ttl() added
